2f92c79d1d1df1ba401bf9748312379915d5756e
Paul Jacobs <paul.jacobs@percona.com>
2021-04-15 14:54:10 +0300

    Page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,47 +8,85 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
 const products = [
-  {
-    title: 'Percona Monitoring and Management',
-    linkUrl: 'https://pmm-doc.netlify.app/',
-    imageUrl: 'https://www.percona.com/sites/default/files/pmm-iso-red.png',
-    description: (
-      <>
-        Platform for monitoring and managing MySQL and MongoDB performance.
+    {
+      title: 'Percona Monitoring and Management',
+      linkUrl: 'https://pmm-doc.netlify.app/',
+      imageUrl: 'https://www.percona.com/sites/default/files/pmm-iso-red.png',
+      imageClass: [''],
+      description: (
+          <>
+      Platform for monitoring and managing MySQL and MongoDB performance.
       </>
-    ),
-  },
-  {
-    title: 'Percona Backup for MongoDB',
-    linkUrl: 'https://percona-backup-mongodb.netlify.app/',
-    imageUrl: 'https://www.percona.com/sites/default/files/backup-mongo.jpg',
-    description: (
-      <>
-        A fully supported, open source, community backup tool for performing consistent hot backups in MongoDB.
+      ),
+    },
+    {
+        title: 'Percona Backup for MongoDB',
+        linkUrl: 'https://percona-backup-mongodb.netlify.app/',
+        imageUrl: 'https://www.percona.com/sites/default/files/backup-mongo.jpg',
+        imageClass: [''],
+        description: (
+            <>
+      A fully supported, open source, community backup tool for performing consistent hot backups in MongoDB.
       </>
-    ),
-  },
-  {
-    title: 'Another Percona Product',
-    linkUrl: 'https://percona-backup-mongodb.netlify.app/',
-    imageUrl: 'https://www.percona.com/sites/default/files/backup-mongo.jpg',
-    description: (
-      <>
-        Description.
-      </>
-    ),
-  }
-];
+      ),
+    },
+    {
+        title: 'Another Percona Product',
+        linkUrl: '',
+        imageUrl: '',
+        imageClass: [''],
+      description: (
+        <>
+          Description.
+        </>
+      ),
+    },
+    {
+        title: 'Another Percona Product',
+        linkUrl: '',
+        imageUrl: '',
+        imageClass: [''],
+      description: (
+        <>
+          Description.
+        </>
+      ),
+    },
+    {
+        title: 'Another Percona Product',
+        linkUrl: '',
+        imageUrl: '',
+        imageClass: [''],
+      description: (
+        <>
+          Description.
+        </>
+      ),
+    },
+    {
+        title: 'Another Percona Product',
+        linkUrl: '',
+        imageUrl: '',
+        imageClass: ['logo-item-downloads-software', 'logo-server-home'],
+      description: (
+        <>
+          Description.
+        </>
+      ),
+    },
+  ];
 
-function Product({linkUrl, imageUrl, title, description}) {
+
+function Product({linkUrl, imageUrl, imageClass, title, description}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-      <div className={classnames('card', styles.product)}>
-      {imgUrl && (<img className={classnames('card-img-top', styles.productImage)} src={imgUrl} alt={title} />)}
-        <div class="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href={linkUrl} class="btn btn-primary">Select</a>
+    <div className={classnames('card', 'sm', 'm-2')}>
+        <div class="col-md">
+            <div class="card-body">
+                <h5 className={classnames('card-title')}>{title}</h5>
+                <p className={classnames('card-text')}>{description}</p>
+                <a href={linkUrl} class="btn btn-light btn-sm active" role="button" aria-pressed="true">Visit</a>
+            </div>
         </div>
     </div>
   );
@@ -57,16 +95,17 @@ function Product({linkUrl, imageUrl, title, description}) {
 export default function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
-
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="Percona products technical documentation"
       >
           {products && products.length > 0 && (
-              <div class="d-flex flex-wrap">
+        <div class='container'>
+            <div class="row no-gutters">
                 {products.map((props, idx) => ( <Product key={idx} {...props} /> ))}
-                 </div>
+                </div>
+            </div>
           )}
     </Layout>
   );
